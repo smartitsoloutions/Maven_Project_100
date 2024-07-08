ee08227a7f41c0dd360f5191e71ad56c9919f4c8
smartitsoloutions <prasads128312@gmail.com>
2024-07-08 17:49:16 +0530

    this is my maven project 100

---
+++ src/main/java/my_first_project_001/project_100/App.java
@@ -0,0 +1,13 @@
+package my_first_project_001.project_100;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
